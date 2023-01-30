32415a69055a31e1d025de6b161f71361d8c666a
Sebastian B <sebbarry@comcast.net>
2023-01-30 15:26:54 -0700

    Added is-docker package
    config.env
    - Added new config variable named DOCKER_MONGO_URI which will be used if the app is ran by docker
    
    db.js
    - imported isDocker dependency
    - Added logic to use DOCKER_MONGO_URI if we are in a docker container, otherwise use the MONGO_URI
    
    package.json
    - Added isDocker package
    
    package-lock.json
    - Added isDocker package

--- root/backend/config/db.js
+++ root/backend/config/db.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
+import isDocker from 'is-docker';
 
 const connectDB = async() => {
     try {
         // Create databaseName and MONGO_URI if config/config.env MONGO_URI is not available
         const databaseName = `my-db`;
-        const MONGO_URI = process.env.MONGO_URI || `mongodb://localhost:27017/${databaseName}`;
+        const MONGO_URI = isDocker()? process.env.DOCKER_MONGO_URI : process.env.MONGO_URI || `mongodb://127.0.0.1:27017/${databaseName}`;
         console.log("Mongo URI: ", MONGO_URI);
         const con = await mongoose.connect(MONGO_URI, { 
             useNewUrlParser: true,
