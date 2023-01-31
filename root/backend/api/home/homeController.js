46ea60a292e8fe31dac067bbfa205de16d93913c
kevinhawekotte <43765314+kevinhawekotte@users.noreply.github.com>
2023-01-31 15:18:18 -0700

    Update root/backend/api/home/homeController.js
    Co-authored-by: Sebastian Barry <61334340+sebastianbarry@users.noreply.github.com>

--- root/backend/api/home/homeController.js
+++ root/backend/api/home/homeController.js
@@ -7,7 +7,7 @@ const router = express.Router();
 export const getHome = async (req, res) => {
   try {
 
-    res.status(200).send("server is running");
+    res.status(200).send("server is running"); //if you do a HTTP GET on localhost:4001, you will get this as a response
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
