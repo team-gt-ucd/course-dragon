53d064c47223b978409273648f9fc2062a575953
Sebastian B <sebbarry@comcast.net>
2023-02-02 10:41:54 -0700

    Update main.jsx to React18
    We were getting a warning for ReactDOM.render, so changed to use createRoot.render

--- root/frontend/src/main.jsx
+++ root/frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+const container = document.getElementById('root')
 
-ReactDOM.render(
+const root = createRoot(container)
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 )
