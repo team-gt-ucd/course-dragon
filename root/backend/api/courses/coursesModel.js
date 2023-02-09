f34300bdb6a90b148d5f1654c1e1006623115cd4
kevinhawekotte <43765314+kevinhawekotte@users.noreply.github.com>
2023-02-08 21:32:40 -0700

    fixed import statement
    added .js

--- root/backend/api/courses/coursesModel.js
+++ root/backend/api/courses/coursesModel.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
-import { creditsSchema } from '../credits/creditsModel';
-import { instructorScoresSchema } from '../instructor-scores/instructorScoresModel';
+import { creditsSchema } from '../credits/creditsModel.js';
+import { instructorScoresSchema } from '../instructor-scores/instructorScoresModel.js';
 export const coursesSchema = mongoose.Schema({
     course_code: String,
     course_title: String,
