26a5610a8a921f1561cab8b960e06b6ea9ce70a7
Nima Sherpa <sherpa2025@gmail.com>
2023-04-19 22:42:27 -0600

    Create userRoute.js

---
+++ root/backend/api/user/userRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import {
+  createUser,
+} from "./userController.js";
+
+const router = express.Router();
+
+
+router.post("/", createUser);
+
+
+export default router;
