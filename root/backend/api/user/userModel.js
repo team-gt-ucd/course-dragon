7d1301b72470bfbb376d19d47c09dba72b842c64
Nima Sherpa <sherpa2025@gmail.com>
2023-04-23 16:14:05 -0600

    Update userModel.js

--- root/backend/api/user/userModel.js
+++ root/backend/api/user/userModel.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
+import encrypt from "mongoose-encryption";
 
 const userSchema = new mongoose.Schema ({
   email: String,
   password: String,
 });
 
+const secret = "Encryptthepassword.";
+userSchema.plugin(encrypt, { secret:secret, encryptedFields: ["password"]});
 
 var UserItem = mongoose.model('useritem', userSchema);
 
